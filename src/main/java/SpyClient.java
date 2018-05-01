c1ef3cd5aefdae4dc0c87adaf0b8d84cea262805
Michal Misiewicz <misiewicz.michall@gmail.com>
2018-05-01 21:44:59 +0200

    clientV2

--- src/main/java/SpyClient.java
+++ src/main/java/SpyClient.java
@@ -2,57 +2,61 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.stream.Collectors;
 
 @Slf4j
 public class SpyClient {
-    BufferedImage bufferedImage;
-    ServerSocket taskSocket;
+    Socket taskSocket;
 
     static final String SERVER_URL = "localhost";
-    static final int CLIENT_PORT = 8081;
     static final int SERVER_PORT = 9999;
 
-    public static void main(String... args) {
+    public static void main(String... args) throws InterruptedException {
         SpyClient client = new SpyClient();
         try {
-            client.taskSocket = new ServerSocket(CLIENT_PORT);
-            Socket serverMessage = client.taskSocket.accept();
-            log.debug("Get new message from: " + serverMessage.getRemoteSocketAddress());
-            String result = new BufferedReader(new InputStreamReader(serverMessage.getInputStream()))
-                    .lines()
-                    .collect(Collectors.joining("\n"));
-            if (result.equals("SPH")) {
-                log.info("Get SPH message, sending photo to server");
-                client.sendImage(SERVER_URL, SERVER_PORT, "/Users/wjanaszek/Downloads/26772171_759476057591382_693966661_o.jpg");
+            client.taskSocket = new Socket(SERVER_URL, SERVER_PORT);
+            DataInputStream dIn = new DataInputStream(client.taskSocket.getInputStream());
+
+            while (true) {
+                byte[] header = new byte[3];
+                dIn.readFully(header, 0, 3);
+                String result = new String(header, StandardCharsets.UTF_8);
+                log.debug("Get new message from: " + SERVER_URL);
+                if (result.equals("SPH")) {
+                    Thread.sleep(2000);
+                    log.info("Get SPH message, sending photo to server");
+                    client.sendImage("/home/michal/Downloads/ted.jpeg");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                client.taskSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    BufferedImage loadImage(String path) {
-        BufferedImage img = null;
-        try {
-            img = ImageIO.read(new File(path));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return img;
+    File loadImage(String path) {
+        return new File(path);
     }
 
-    void sendImage(String url, int port, String path) {
+    void sendImage(String path) {
         try {
-            Socket socket = new Socket(url, port);
-            bufferedImage = this.loadImage(path);
-            ImageIO.write(bufferedImage, path.split(".")[0], socket.getOutputStream());
-            socket.close();
+            File file = this.loadImage(path);
+            byte[] fileContent = Files.readAllBytes(file.toPath());
+            DataOutputStream dos = new DataOutputStream(taskSocket.getOutputStream());
+            dos.writeInt(fileContent.length);
+            dos.write(fileContent);
+            dos.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
